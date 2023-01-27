15e2b997afbc19102d5705dac7ae51ea1f85ac01
Aluno da Infinity School <aluno@3D.local>
2023-01-26 21:53:57 -0300

    Api finalizada

--- app.py
+++ app.py
@@ -1,24 +1,28 @@
 from flask import Flask, request, jsonify, Response
 from resource.models.all_models import *
 from resource.database import livraria_repository
+import http.client as HTTP_STATUS
 
 
 app = Flask(__name__)
 
 
+# Read All
 @app.route('/livros', methods=['GET'])
 def get_all_books():
     livros = livraria_repository.get_all()
     return livros if livros is not None else Response(status=204)
 
+# Read One
 @app.route('/livros/<int:id>', methods=['GET'])
 def get_one_book(id):
     livro = livraria_repository.get_by_id(id)
     if livro is None:
-        return Response(status=204)
+        return Response(status = HTTP_STATUS.NO_CONTENT)
     return jsonify(livro.to_json())     
 
-@app.route('/livros', methods=['POST'])
+# Create
+@app.route('/livros/adicionar', methods=['POST'])
 def cadastrar_livro():
     livro_data = request.get_json()
     livro = Livro(
@@ -29,20 +33,45 @@ def cadastrar_livro():
 
     try:
         livraria_repository.inserir_livro(livro)
-    except:
-        return Response(
-                response = {"message" : "Erro ao cadastrar o livro"},
-                status = 500
-            )
+    except Exception as err:
+        return Response(status = HTTP_STATUS.BAD_REQUEST)
 
-    return Response (
-            response = {"message" : "Livro Cadastrado com Sucesso"},
-            status = 200
-        )
+    return Response(status = HTTP_STATUS.CREATED)
 
-@app.route('/livros/<int:id>', methods=['PUT'])
+# Update
+@app.route('/livros/alterar/<int:id>', methods=['PATCH'])
 def alterar_livro(id):
-    pass
+    livro_data = request.get_json()
+    livro_to_update = livraria_repository.get_by_id(id)
+    
+    if livro_to_update is None:
+        return Response(status = HTTP_STATUS.NO_CONTENT)
+    
+    try:
+        new_livro = Livro(
+            nome= livro_data['nome'],
+            ano= livro_data['ano'],
+            autor= livro_data['autor']
+        )
+        livraria_repository.alterar_livro(id, new_livro)
+    except Exception as err:
+        return Response(status = HTTP_STATUS.BAD_REQUEST)
+    
+    return Response(status = HTTP_STATUS.CREATED)
+
+# Delete
+@app.route('/livros/deletar/<int:id>', methods=['DELETE'])
+def deletar_livro(id):
+    try:
+        livraria_repository.deletar_livro(id)
+    except Exception as err:
+        return Response(
+                response = {"message" : err.__cause__},
+                status = HTTP_STATUS.BAD_REQUEST
+            )
+   
+    return Response(status = HTTP_STATUS.ACCEPTED)
+        
 
 if __name__ == '__main__':
     app.run(debug=True)
--- resource/database/livraria_repository.py
+++ resource/database/livraria_repository.py
@@ -1,24 +1,49 @@
-from resource.connection.session_db import session_factory
+from resource.connection.session_factory import session_factory
 from resource.models.Livro import Livro
 from typing import Optional
 
 
-def inserir_livro(livro: Livro):
+def get_all() -> Optional[list[Livro]]:
+    with session_factory() as session:
+        livros = [livro.to_json() for livro in session.query(Livro).all()]
+        return livros if len(livros) > 0 else None
+
+def get_by_id(id: int) -> Optional[Livro]:
+    with session_factory() as session:
+        livro = session.query(Livro).get(id)
+    return livro
+
+def inserir_livro(livro: Livro) -> None:
     with session_factory() as session:
         try: 
             session.add(livro)
             session.commit()
         except:
             session.rollback()
-            raise Exception("Erro ao adicionar item")
+            raise Exception('Erro ao adicionar item')
 
-def get_by_id(id: int) -> Livro:
-    with session_factory() as session:
-        livro = session.query(Livro).get(id)
+def alterar_livro(id: int, livro: Livro) -> None:
+    livro_to_update = get_by_id(id)
     
-    return livro
-
-def get_all() -> Optional[list[Livro]]:
     with session_factory() as session:
-        livros = [livro.to_json() for livro in session.query(Livro).all()]
-        return livros if len(livros) > 0 else None
+        try:
+            livro_to_update.nome = livro.nome
+            livro_to_update.ano = livro.ano
+            livro_to_update.autor = livro.autor
+            session.commit()
+        except:
+            session.rollback()
+            raise Exception('Erro ao alterar livro')
+
+def deletar_livro(id):
+    livro = get_by_id(id)
+    if livro is None: 
+        raise Exception('Não foi possivel encontrar o livro')
+    
+    with session_factory() as session:
+        try:
+            session.delete(livro)
+            session.commit
+        except:
+            session.rollback()
+            raise Exception('Houve um erro ao deletar o livro')
